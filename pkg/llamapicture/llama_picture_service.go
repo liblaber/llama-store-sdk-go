382849838c97f6c9ae542c316520ca53b1ed0fa4
Jim Bennett <jimbobbennett@liblab.com>
2024-03-12 04:37:38 -0700

    SDK update generated by liblab

--- pkg/llamapicture/llama_picture_service.go
+++ pkg/llamapicture/llama_picture_service.go
@@ -1,10 +1,10 @@
 package llamapicture
 
 import (
+	"context"
 	restClient "github.com/liblaber/llama-store-sdk-go/internal/clients/rest"
 	"github.com/liblaber/llama-store-sdk-go/internal/clients/rest/httptransport"
 	"github.com/liblaber/llama-store-sdk-go/internal/configmanager"
-	"github.com/liblaber/llama-store-sdk-go/internal/unmarshal"
 	"github.com/liblaber/llama-store-sdk-go/pkg/llamastoreconfig"
 	"github.com/liblaber/llama-store-sdk-go/pkg/shared"
 )
@@ -28,53 +28,47 @@ func (api *LlamaPictureService) SetBaseUrl(baseUrl string) {
 	config.SetBaseUrl(baseUrl)
 }
 
+func (api *LlamaPictureService) SetAccessToken(accessToken string) {
+	config := api.getConfig()
+	config.SetAccessToken(accessToken)
+}
+
 // Get a llama's picture by the llama ID. Pictures are in PNG format.
-func (api *LlamaPictureService) GetLlamaPictureByLlamaId(llamaId int64) error {
+func (api *LlamaPictureService) GetLlamaPictureByLlamaId(ctx context.Context, llamaId int64) (*shared.LlamaStoreResponse[[]byte], *shared.LlamaStoreError) {
 	config := *api.getConfig()
 
-	client := restClient.NewRestClient(config)
+	client := restClient.NewRestClient[[]byte](config)
 
-	request := httptransport.NewRequest("GET", "/llama/{llama_id}/picture", config)
+	request := httptransport.NewRequest(ctx, "GET", "/llama/{llama_id}/picture", config)
 
 	request.SetPathParam("llama_id", llamaId)
 
-	_, err := client.Call(request)
+	resp, err := client.Call(request)
 	if err != nil {
-		return err.GetError()
+		return nil, shared.NewLlamaStoreError[[]byte](err)
 	}
 
-	return nil
+	return shared.NewLlamaStoreResponse[[]byte](resp), nil
 }
 
 // Create a picture for a llama. The picture is sent as a PNG as binary data in the body of the request.
-func (api *LlamaPictureService) CreateLlamaPicture(llamaId int64) (*shared.LlamaStoreResponse[LlamaId], error) {
+func (api *LlamaPictureService) CreateLlamaPicture(ctx context.Context, llamaId int64, body []byte) (*shared.LlamaStoreResponse[LlamaId], *shared.LlamaStoreError) {
 	config := *api.getConfig()
 
-	client := restClient.NewRestClient(config)
+	client := restClient.NewRestClient[LlamaId](config)
 
-	request := httptransport.NewRequest("POST", "/llama/{llama_id}/picture", config)
+	request := httptransport.NewRequest(ctx, "POST", "/llama/{llama_id}/picture", config)
+
+	request.Body = body
 
 	request.SetPathParam("llama_id", llamaId)
 
-	httpResponse, err := client.Call(request)
+	resp, err := client.Call(request)
 	if err != nil {
-		return nil, err.GetError()
+		return nil, shared.NewLlamaStoreError[LlamaId](err)
 	}
 
-	data, unmarshalError := unmarshal.ToObject[LlamaId](httpResponse)
-	if unmarshalError != nil {
-		return nil, unmarshalError
-	}
-
-	response := shared.LlamaStoreResponse[LlamaId]{
-		Data: *data,
-		Metadata: shared.LlamaStoreResponseMetadata{
-			Headers:    httpResponse.Headers,
-			StatusCode: httpResponse.StatusCode,
-		},
-	}
-
-	return &response, nil
+	return shared.NewLlamaStoreResponse[LlamaId](resp), nil
 }
 
 // Update a picture for a llama. The picture is sent as a PNG as binary data in the body of the request.
@@ -82,50 +76,39 @@ func (api *LlamaPictureService) CreateLlamaPicture(llamaId int64) (*shared.Llama
 // If the llama does not have a picture, one will be created. If the llama already has a picture,
 // it will be overwritten.
 // If the llama does not exist, a 404 will be returned.
-func (api *LlamaPictureService) UpdateLlamaPicture(llamaId int64) (*shared.LlamaStoreResponse[LlamaId], error) {
+func (api *LlamaPictureService) UpdateLlamaPicture(ctx context.Context, llamaId int64, body []byte) (*shared.LlamaStoreResponse[LlamaId], *shared.LlamaStoreError) {
 	config := *api.getConfig()
 
-	client := restClient.NewRestClient(config)
+	client := restClient.NewRestClient[LlamaId](config)
 
-	request := httptransport.NewRequest("PUT", "/llama/{llama_id}/picture", config)
+	request := httptransport.NewRequest(ctx, "PUT", "/llama/{llama_id}/picture", config)
+
+	request.Body = body
 
 	request.SetPathParam("llama_id", llamaId)
 
-	httpResponse, err := client.Call(request)
+	resp, err := client.Call(request)
 	if err != nil {
-		return nil, err.GetError()
+		return nil, shared.NewLlamaStoreError[LlamaId](err)
 	}
 
-	data, unmarshalError := unmarshal.ToObject[LlamaId](httpResponse)
-	if unmarshalError != nil {
-		return nil, unmarshalError
-	}
-
-	response := shared.LlamaStoreResponse[LlamaId]{
-		Data: *data,
-		Metadata: shared.LlamaStoreResponseMetadata{
-			Headers:    httpResponse.Headers,
-			StatusCode: httpResponse.StatusCode,
-		},
-	}
-
-	return &response, nil
+	return shared.NewLlamaStoreResponse[LlamaId](resp), nil
 }
 
 // Delete a llama's picture by ID.
-func (api *LlamaPictureService) DeleteLlamaPicture(llamaId int64) error {
+func (api *LlamaPictureService) DeleteLlamaPicture(ctx context.Context, llamaId int64) (*shared.LlamaStoreResponse[any], *shared.LlamaStoreError) {
 	config := *api.getConfig()
 
-	client := restClient.NewRestClient(config)
+	client := restClient.NewRestClient[any](config)
 
-	request := httptransport.NewRequest("DELETE", "/llama/{llama_id}/picture", config)
+	request := httptransport.NewRequest(ctx, "DELETE", "/llama/{llama_id}/picture", config)
 
 	request.SetPathParam("llama_id", llamaId)
 
-	_, err := client.Call(request)
+	resp, err := client.Call(request)
 	if err != nil {
-		return err.GetError()
+		return nil, shared.NewLlamaStoreError[any](err)
 	}
 
-	return nil
+	return shared.NewLlamaStoreResponse[any](resp), nil
 }
